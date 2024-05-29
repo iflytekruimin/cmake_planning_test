b66b9f5c15d28e03c144e25738a656410b611508
CPPyyds <994505438@qq.com>
2024-05-29 17:35:04 +0800

    version 0.0.1

---
+++ pnc_map.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "pnc_map.h"
+using namespace std;
+
+void PNC_MAP::mapInfo()
+{
+    cout<<"thisispnc_map"<<endl;
+}
---
+++ process.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "process.h"
+using namespace std;
+void Process::planProcess()
+{
+    cout<<"thisisplaningprocess"<<endl;
+    my_map.mapInfo();
+    cout<<"planning success!"<<endl;
+};
