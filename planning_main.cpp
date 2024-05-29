4e53ed2bf288cff8e7a9423bcd2a81bd1426ed7f
CPPyyds <994505438@qq.com>
2024-05-29 21:08:13 +0800

    test ignore

--- planning_main.cpp
+++ planning_main.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout<<"planing start"<<endl;
+    
+    cout<<"planning start"<<endl;
     Process pro;
     pro.planProcess();
-    cout<<"planing end"<<endl;
+    cout<<"planning end"<<endl;
     return 0;
 }
